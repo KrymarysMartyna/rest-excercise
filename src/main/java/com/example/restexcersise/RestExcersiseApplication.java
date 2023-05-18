d9ce5481736e4bc7be7d22587686aa58ea5825f4
KrymarysMartyna <dev.martynakrymarys@gmail.com>
2023-05-18 19:30:33 +0200

    added application draft

---
+++ src/main/java/com/example/restexcersise/RestExcersiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.restexcersise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestExcersiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestExcersiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restexcersise/RestExcersiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restexcersise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestExcersiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
